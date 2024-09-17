d0fd7598c37a9d5eca02647371ac39fd942bf2bd
alzinan1234 <pappurohan1100@gmail.com>
2024-09-17 20:25:26 +0600

    add

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from "react";
-import pdf from "../pdf/resume.pdf";
+import pdf from "../pdf/Zinan-Resume (4).pdf";
 import hero from "./data/hero.json";
 import Typed from "typed.js";
 
